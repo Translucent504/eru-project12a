dc733a84f9454e5c24e2d4fcba1f76651ce03bf0
Translucent504 <fleshlime.1@gmail.com>
2020-10-29 09:43:26 +0500

    removed file sourcing

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
